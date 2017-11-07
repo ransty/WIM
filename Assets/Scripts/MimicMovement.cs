127ab96b29d407df9dd319d8ad848e8934bc851c
Keano Porcaro <keano.porcaro@gmail.com>
2017-11-07 12:39:35 +1030

    WIM 1.0
    Rolling Blocks and WIM :)

---
+++ Assets/Scripts/MimicMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimicMovement : MonoBehaviour {
+
+    // place on WIM object
+    public GameObject otherObject; // the object in the room
+    public float scale = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        otherObject.transform.localPosition = this.transform.localPosition / scale;
+        otherObject.transform.localRotation = this.transform.localRotation;
+	}
+}
